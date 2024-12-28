e074a8c26d324509f29d0334bbe4c7ac050a9e00
William <wigsinator@gmail.com>
2024-12-28 17:47:17 -0500

    Fixed aStar implementation

--- help/aStar.go
+++ help/aStar.go
@@ -6,34 +6,38 @@ import (
 
 type heuristic func(Pos) int
 
-type distance func(Pos, Pos) int
+type distance func(Pos, Pos) (int, bool)
 
-func AStar(start Pos, goal Pos, h heuristic, d distance) { 
+func AStar(start Pos, goal Pos, h heuristic, d distance) []Pos { 
 
   cameFrom := make(map[Pos]Pos)
 
   gScore := make(map[Pos]int)
-  gScore[start] := 0
+  gScore[start] = 0
 
   fScore := make(map[Pos]int)
-  fScore[start] := h(start)
+  fScore[start] = h(start)
 
-  openSet := newHeap[Pos](func(a,b Pos) bool {return fScore[a] < fScore[b]})
+  openSet := NewHeap[Pos](func(a,b Pos) bool {return fScore[a] < fScore[b]})
+  openSet.Push(start)
 
   for openSet.Len() > 0 {
     current := openSet.Pop()
     if current == goal {
-      return rebuild_path(cameFrom, current)
+      return rebuild_path(cameFrom, current, start)
     }
 
-    openSet.Remove(current)
     for _,neighbor := range current.Neighbors() {
-      tentative_gScore := gScore[current] + d(current, neighbor)
+      dist, success := d(current, neighbor)
+      if !success {
+        continue
+      }
+      tentative_gScore := gScore[current] + dist
       if val, ok := gScore[neighbor]; !ok || tentative_gScore < val {
         cameFrom[neighbor] = current
         gScore[neighbor] = tentative_gScore
         fScore[neighbor] = tentative_gScore + h(neighbor)
-        if neighbor not in openSet {
+        if !openSet.Exists(neighbor) {
           openSet.Push(neighbor)
         }
       }
